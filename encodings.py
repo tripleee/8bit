a08525c6708ce627fc51ee73f329cb040525ff8a
tripleee <tripleee@users.noreply.github.com>
2014-11-24 12:25:39 +0200

    encodings.py: Simple script to create mapping
    README.md: Updated with license and sample output

---
+++ encodings.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from collections import defaultdict
+
+def encodings():
+    '''http://stackoverflow.com/a/1728414/874188'''
+    import pkgutil
+    import encodings
+
+    exclude=set(['aliases',
+        # Exclude binary encodings
+        'bz2_codec', 'punycode', 'hex_codec', 'uu_codec', 'unicode_internal',
+        'quopri_codec', 'raw_unicode_escape', 'unicode_escape', 'base64_codec',
+        'zlib_codec', 'charmap', 'ascii', 'string_escape', 'rot_13',
+        'undefined', 'idna',
+        # Exclude multi-byte encodings: UTF-xx
+        'mbcs', 'utf_7', 'utf_8', 'utf_8_sig',
+        'utf_16', 'utf_16_be', 'utf_16_le',
+        'utf_32', 'utf_32_be', 'utf_32_le',
+        # Chinese
+        'big5hkscs', 'gbk', 'gb2312', 'hz', 'big5', 'gb18030', 'cp950',
+        # Japanese
+        'euc_jp', 'euc_jisx0213', 'euc_jis_2004',
+        'iso2022_jp', 'iso2022_jp_ext', 'iso2022_jp_1',
+        'iso2022_jp_2', 'iso2022_jp_3', 'iso2022_jp_2004',
+        'shift_jis', 'shift_jis_2004', 'shift_jisx0213', 'cp932',
+        # Korean
+        'euc_kr', 'iso2022_kr', 'johab', 'cp949'])
+
+    found=set(name for im, name, ispkg in
+        pkgutil.iter_modules(encodings.__path__))
+    found.difference_update(exclude)
+    return found
+
+codecs = encodings()
+result = dict()
+
+for ch in xrange(128,256,1):
+    print("0x%02x" % ch)
+    char = chr(ch)
+    result[ch] = defaultdict(list)
+    for enc in codecs:
+        try:
+            code = char.decode(enc)
+            result[ch][code].append(enc)
+        except UnicodeDecodeError, err:
+            if 'character maps to <undefined>' in err:
+                result[ch]['undefined'].append(enc)
+            else:
+                raise
+    for glyph in sorted(result[ch].keys()):
+        if glyph == 'undefined':
+            continue
+        print(("  %s (%r): %s" % (glyph, glyph, ", ".join(result[ch][glyph]))).encode('utf-8'))
+    if 'undefined' in result[ch]:
+        print("  (undefined): %s" % ", ".join(result[ch]['undefined']))
+    print("")
