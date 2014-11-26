cc222a719e0ad0acf497269eb6bb347697aa8d14
tripleee <tripleee@users.noreply.github.com>
2014-11-26 14:08:11 +0200

    encodings.py: Include date, Python version, host
    The HTML page includes this information in a remark before the tables
    of generated output.

--- encodings.py
+++ encodings.py
@@ -4,6 +4,8 @@
 from collections import defaultdict
 from textwrap import TextWrapper
 from optparse import OptionParser
+from platform import python_version, node
+from time import strftime
 
 
 parser = OptionParser()
@@ -78,7 +80,9 @@ if options.html:
       <p>You can link to individual character codes with an anchor
       like <a href="#0xaf">encodings.html#0xaf</a> -- just edit the link
       in the Location: bar of your browser.</p>
-''')
+      <hr>
+      <p>This page was generated on %s by Python %s on host <tt>%s</tt>.</p>
+''' % (strftime('%c'), python_version(), node().split('.')[0]))
     title = lambda x: '<a name="%s"><h3>%s</h3></a>\n<p><table>' % (x, x)
     row = lambda x: '<tr><td>%s</td><td>%s</td>\n' % (x[0], x[1])
     enddiv = lambda: '</table>'
